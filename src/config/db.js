07e13cdc5a905e3460de6aff023344cc4e14210f
shanika200721 <shanika.uok2@gmail.com>
2025-10-07 18:41:19 +0530

    added .env.example file and chosted database

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT || 3306),            // add the number 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME,
@@ -11,6 +11,7 @@ const pool = mysql.createPool({
   queueLimit: 0,
   //FOR CORRECT TIME PARSING
   dateStrings: true,
+  ssl: {},
 });
 
-module.exports = pool;
+module.exports=pool;
