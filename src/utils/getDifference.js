d023af9a0a89054211480601df1d4742df10ee54
Vipusa Sriharan <develsupport06@cyberMasit.com>
2025-12-01 11:07:09 +0530

    department and announcement CRUD operations

--- src/utils/getDifference.js
+++ src/utils/getDifference.js
@@ -1,10 +1,19 @@
- function getDifference(before, after) {
+function getDifference(before, after) {
   const diff = {};
+
+  if (!before) {
+    for (const key in after) {
+      diff[key] = { before: null, after: after[key] };
+    }
+    return diff;
+  }
+
   for (const key in after) {
     if (JSON.stringify(before[key]) !== JSON.stringify(after[key])) {
       diff[key] = { before: before[key], after: after[key] };
     }
   }
+
   return diff;
 }
 
